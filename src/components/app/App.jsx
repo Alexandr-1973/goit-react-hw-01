7e8eb828542d75b2fb9510e61a6acc1d7a4a2857
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-03-15 17:59:59 +0200

    commens-app

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
-import userData from '../userData.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+// import userData from '../userData.json';
+// import friends from '../friends.json';
+// import transactions from '../transactions.json';
 
 function App() {
   return <></>;
