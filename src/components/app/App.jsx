23383c192b23bf99964a0d4a733a203c3906e61b
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-03-15 16:54:21 +0200

    json

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,7 @@
 import './App.css';
+import userData from '../userData.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
 
 function App() {
   return <></>;
